0eff86d024946ca2739af4ec49800ef98f10d933
Daveena <kellyangel2330@gmail.com>
2024-07-30 13:32:40 +0200

    update the second modal

--- FrontEnd/assets/script/index.js
+++ FrontEnd/assets/script/index.js
@@ -4,6 +4,7 @@
 const works = [];
 let deleteBtn = [];
 let token;
+const URL = 'http://localhost:5678/api/';
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -112,18 +113,91 @@ document.getElementById('open-modal').addEventListener('click', function () {
 
 })
 
-document.getElementById('open-second-modal').addEventListener('click', () => {
-    document.getElementById('modal').style.display = 'none';
-    document.getElementById('modal').style.opacity = 0;
-    
-    document.getElementById('second-modal').style.display = 'block';
-    document.getElementById('second-modal').style.opacity = 1;
-});
+/********************** MODAL 2 ************************ Pour implémenter les fonctionnalités des boutons de retour et de fermeture dans la modal,*/
+const openSecondModal = () => {
+    document.getElementById("overflow-modal").style.display = "block";
+    document.getElementById("overflow-modal").style.opacity = 1;
+    document.getElementById("modal").style.display = "none";
+    document.getElementById("modal").style.opacity = 0;
+}
 
-document.getElementById('close-second-modal').addEventListener('click', () => {
-    document.getElementById('second-modal').style.display = 'none';
-    document.getElementById('second-modal').style.opacity = 0;
-});
+const closeSecondModal = () => {
+    document.getElementById("overflow-modal").style.display = "none";
+    document.getElementById("overflow-modal").style.opacity = 0;
+}
+
+
+const returnToModal = () => {
+    document.getElementById("overflow-modal").style.display = "none";
+    document.getElementById("overflow-modal").style.opacity = 0;
+    document.getElementById("modal").style.display = "block";
+    document.getElementById("modal").style.opacity = 1;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('imageContainer').style.display = 'none';
+  });
+
+let file;
+const selectImage = () => {
+  const fileInput = document.getElementById('fileInput');
+
+  fileInput.addEventListener('change', (event) => {
+    event.stopPropagation();
+    file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const imgElement = document.createElement('img');
+        imgElement.src = e.target.result;
+        imgElement.style.maxWidth = '100%';
+        document.getElementById('imageContainer').innerHTML = '';
+        document.getElementById('imageContainer').appendChild(imgElement);
+        document.getElementById('imageIcon').style.display = 'none';
+        document.getElementById('imageContainer').style.display = 'block';
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+
+  fileInput.click();
+};
+
+document
+  .getElementById('create-works-form')
+  .addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const category = document.getElementById('category').value;
+
+    const url = URL + 'works';
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('category', category);
+    formData.append('image', file);
+
+    try {
+      await fetch(url, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      });
+
+      if (!response.ok) {
+        console.log(response);
+      }
+    } catch (error) {
+      console.error('Failed to create work:', error);
+    }
+  });
+
+
+
+
 
 
 
