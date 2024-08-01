0226902063f689f1ac986468eb8023840d072105
Daveena <kellyangel2330@gmail.com>
2024-08-01 14:36:26 +0200

    update and reorganize  index.js file

--- FrontEnd/assets/script/index.js
+++ FrontEnd/assets/script/index.js
@@ -1,144 +1,129 @@
 "use strict";
 
 // ********** CONSTANTS ********** //
-const works = [];
-let deleteBtn = [];
-let token;
+
 const URL = 'http://localhost:5678/api/';
 
+const worksElt      = document.getElementById('work');
+const categoriesElt = document.getElementById('categories');
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Code pour lancer l'application
-    console.log('Application en cours de chargement...');
-    token = localStorage.getItem('token');
-    if (token) {
-        document.getElementById('login-url').style.display = 'none';
-        document.getElementById('logout').style.display = 'block';
-    }
-})
+// ********** VARIABLES ********** //
 
-document.getElementById('logout').addEventListener('click', function () {
-    localStorage.removeItem('token');
-    document.getElementById('login-url').style.display = 'block';
-    document.getElementById('logout').style.display = 'none';
-})
+let works = [];
+let categories = [];
 
-const getWorks = async () => {
-    const worksdoc = document.getElementById('mywork');
+let token = '';
+let file;
 
-    try {
-        const response = await fetch('http://localhost:5678/api/works');
-        const data = await response.json();
-        works.push(...data);
+// ********** FUNCTIONS ********** //
 
-        works.forEach((work) => {
-            const figure = document.createElement('figure');
-            figure.innerHTML = `
-                <img src="${work.imageUrl}" alt="${work.title}">
-                <figcaption>${work.title}</figcaption>
-            `;
-            worksdoc.appendChild(figure);
-        });
+// DATA
 
-    } catch (error) {
-        console.log(error);
-    }
+const getData = async (type) => {
+  try {
+    const response = await fetch(URL + type);
+    const data = await response.json();
+    console.log(data);
 
-}
-document.addEventListener('DOMContentLoaded', getWorks)
+    return data;
 
-
-const getCategories = async () => {
-    const categoriesDoc = document.getElementById('categories');
-
-    try {
-        const response = await fetch('http://localhost:5678/api/categories');
-        const categories = await response.json();
-        console.log(categories);
-
-        categories.forEach((category) => {
-            const categoryElement = document.createElement('button');
-            categoryElement.textContent = category.name;
-            categoriesDoc.appendChild(categoryElement);
-        });
-
-    } catch (error) {
-        console.log(error);
-    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', getCategories)
+const displayWorks = async () => {
+  works = await getData('works');
 
-document.getElementById('close-modal').addEventListener('click', function () {
-    document.getElementById('modal').style.display = 'none';
-    document.getElementById('modal').style.opacity = 0;
+  for (const work of works) {
+    const figure = document.createElement('figure');
 
-})
+    figure.innerHTML = `
+      <img src="${work.imageUrl}" alt="${work.title}">
+      <figcaption>${work.title}</figcaption>
+    `;
 
-document.getElementById('open-modal').addEventListener('click', function () {
-    document.getElementById('modal').style.display = 'block';
-    document.getElementById('modal').style.opacity = 1;
+    worksElt.appendChild(figure);
+  }
+}
 
-    const modalImages = document.getElementById('modal-images')
+const displayCategories = async () => {
+  categories = await getData('categories');
 
-    works.forEach((work) => {
-        const li = document.createElement('li');
-        li.innerHTML = `
+  for (const category of categories) {
+    const categoryElt = document.createElement('button');
+    categoryElt.textContent = category.name;
+    categoriesElt.appendChild(categoryElt);
+  }
+}
+
+// FIRST MODAL
+
+const openModal = () => {
+  let deleteBtn = [];
+
+  document.getElementById('modal').style.display = 'block';
+  document.getElementById('modal').style.opacity = 1;
+
+  const modalImages = document.getElementById('modal-images')
+
+  works.forEach((work) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
             <img src="${work.imageUrl}" alt="${work.title}">
             <span class="delete"><i class="fa-solid fa-trash-can"></i></span>
         `;
-        modalImages.appendChild(li);
-    });
-    const deleteWorks = document.getElementsByClassName('delete');
-    console.log(deleteWorks);
-    deleteBtn = [...deleteWorks]
+    modalImages.appendChild(li);
+  });
+  const deleteWorks = document.getElementsByClassName('delete');
+  console.log(deleteWorks);
+  deleteBtn = [...deleteWorks]
 
-    for (let i = 0; i < deleteBtn.length; i++) {
-        deleteBtn[i].addEventListener('click', async function () {
-            if (confirm('Voulez-vous supprimer cette image ?')) {
+  for (let i = 0; i < deleteBtn.length; i++) {
+    deleteBtn[i].addEventListener('click', async function () {
+      if (confirm('Voulez-vous supprimer cette image ?')) {
 
-                try {
-                    const response = await fetch(`http://localhost:5678/api/works/${works[i].id}`, {
-                        method: 'DELETE',
-                        headers: {
-                            'Authorization': `Bearer ${token}`
-                        }
-                    })
-                } catch (error) {
-
-                }
+        try {
+          const response = await fetch(`http://localhost:5678/api/works/${works[i].id}`, {
+            method: 'DELETE',
+            headers: {
+              'Authorization': `Bearer ${token}`
             }
-        })
-    }
+          })
+        } catch (error) {
 
-})
+        }
+      }
+    })
+  }
+}
 
-/********************** MODAL 2 ************************ Pour implémenter les fonctionnalités des boutons de retour et de fermeture dans la modal,*/
+const closeModal = () => {
+  document.getElementById('modal').style.display = 'none';
+  document.getElementById('modal').style.opacity = 0;
+}
+
+const returnToModal = () => {
+  document.getElementById("overflow-modal").style.display = "none";
+  document.getElementById("overflow-modal").style.opacity = 0;
+  document.getElementById("modal").style.display = "block";
+  document.getElementById("modal").style.opacity = 1;
+}
+
+// SECOND MODAL
+
 const openSecondModal = () => {
-    document.getElementById("overflow-modal").style.display = "block";
-    document.getElementById("overflow-modal").style.opacity = 1;
-    document.getElementById("modal").style.display = "none";
-    document.getElementById("modal").style.opacity = 0;
+  document.getElementById("overflow-modal").style.display = "block";
+  document.getElementById("overflow-modal").style.opacity = 1;
+  document.getElementById("modal").style.display = "none";
+  document.getElementById("modal").style.opacity = 0;
 }
 
 const closeSecondModal = () => {
-    document.getElementById("overflow-modal").style.display = "none";
-    document.getElementById("overflow-modal").style.opacity = 0;
+  document.getElementById("overflow-modal").style.display = "none";
+  document.getElementById("overflow-modal").style.opacity = 0;
 }
 
-
-const returnToModal = () => {
-    document.getElementById("overflow-modal").style.display = "none";
-    document.getElementById("overflow-modal").style.opacity = 0;
-    document.getElementById("modal").style.display = "block";
-    document.getElementById("modal").style.opacity = 1;
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('imageContainer').style.display = 'none';
-  });
-
-let file;
 const selectImage = () => {
   const fileInput = document.getElementById('fileInput');
 
@@ -164,35 +149,72 @@ const selectImage = () => {
   fileInput.click();
 };
 
+const submitWork = async (event) => {
+  event.preventDefault();
+
+  const title = document.getElementById('title').value;
+  const category = document.getElementById('category').value;
+
+  const url = URL + 'works';
+  const formData = new FormData();
+  formData.append('title', title);
+  formData.append('category', category);
+  formData.append('image', file);
+
+  try {
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
+
+    if (!response.ok) {
+      console.log(response);
+    }
+  } catch (error) {
+    console.error('Failed to create work:', error);
+  }
+}
+
+// RUN
+
+const logout = () => {
+  localStorage.removeItem('token');
+  document.getElementById('login-url').style.display = 'block';
+  document.getElementById('logout').style.display = 'none';
+}
+
+const addListeners = () => {
+  document.getElementById('logout').addEventListener('click', logout);
+  document.getElementById('close-modal').addEventListener('click', closeModal);
+  document.getElementById('open-modal').addEventListener('click', openModal);
+}
+
+// ********** MAIN ********** //
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Application en cours de chargement...');
+  token = localStorage.getItem('token');
+
+  if (token) {
+    document.getElementById('login-url').style.display = 'none';
+    document.getElementById('logout').style.display = 'block';
+  } else {
+
+  }
+
+  displayWorks();
+  displayCategories();
+
+  document.getElementById('imageContainer').style.display = 'none';
+})
+
 document
   .getElementById('create-works-form')
   .addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const title = document.getElementById('title').value;
-    const category = document.getElementById('category').value;
-
-    const url = URL + 'works';
-    const formData = new FormData();
-    formData.append('title', title);
-    formData.append('category', category);
-    formData.append('image', file);
-
-    try {
-      await fetch(url, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        body: formData,
-      });
-
-      if (!response.ok) {
-        console.log(response);
-      }
-    } catch (error) {
-      console.error('Failed to create work:', error);
-    }
+    submitWork(event);
   });
 
 
