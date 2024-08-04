4df09fd88a7bee7699d444c5b5f488bac210b1b3
Daveena <kellyangel2330@gmail.com>
2024-08-04 17:30:32 +0200

    update the filter button, remove the on click in the html and code injected in the js file

--- FrontEnd/assets/script/index.js
+++ FrontEnd/assets/script/index.js
@@ -52,6 +52,13 @@ const displayCategories = async () => {
 
   for (const category of categories) {
     const categoryElt = document.createElement('button');
+
+    categoryElt.setAttribute('data-category', category.id);
+    categoryElt.className = 'category__btn';
+    categoryElt.addEventListener('click', () =>
+      filterWorks(category.id, categoryElt)
+    );
+
     categoryElt.textContent = category.name;
     categoriesElt.appendChild(categoryElt);
   }
@@ -110,6 +117,10 @@ const returnToModal = () => {
   document.getElementById("modal").style.opacity = 1;
 }
 
+document
+  .getElementById('return-second-modal')
+  .addEventListener('click', returnToModal);
+
 // SECOND MODAL
 
 const openSecondModal = () => {
@@ -117,12 +128,33 @@ const openSecondModal = () => {
   document.getElementById("overflow-modal").style.opacity = 1;
   document.getElementById("modal").style.display = "none";
   document.getElementById("modal").style.opacity = 0;
-}
+
+
+const categoriesElement = document.getElementById('category');
+
+  categoriesElement.innerHTML = '';
+
+  for (const category of categories) {
+    const option = document.createElement('option');
+    option.textContent = category.name;
+    option.setAttribute('value', category.id);
+
+    categoriesElement.appendChild(option);
+  }
+};
+
+
+
+document.getElementById('add-photo').addEventListener('click', openSecondModal);
 
 const closeSecondModal = () => {
   document.getElementById("overflow-modal").style.display = "none";
   document.getElementById("overflow-modal").style.opacity = 0;
 }
+
+document
+  .getElementById('second-close-modal')
+  .addEventListener('click', closeSecondModal);
 
 const selectImage = () => {
   const fileInput = document.getElementById('fileInput');
@@ -149,59 +181,110 @@ const selectImage = () => {
   fileInput.click();
 };
 
+
+// FOUNCTION CHANGE BUTTON COLOR
+document.addEventListener('DOMContentLoaded', function () {
+  const titleInput = document.getElementById('title');
+  const fileInput = document.getElementById('fileInput');
+  const categorySelect = document.getElementById('category');
+  const submitButton = document.getElementById('create-works');
+
+  function checkInputs() {
+    const titleValue = titleInput.value.trim();
+    const categoryValue = categorySelect.value.trim();
+    const fileValue = fileInput.files.length > 0;
+
+    if (titleValue !== '' && categoryValue !== '' && fileValue) {
+      submitButton.disabled = false;
+      submitButton.classList.remove('unenabled-button');
+      submitButton.classList.add('enabled-button');
+    } else {
+      submitButton.disabled = true;
+      submitButton.classList.add('unenabled-button');
+      submitButton.classList.remove('enabled-button');
+    }
+  }
+
+  titleInput.addEventListener('input', checkInputs);
+  categorySelect.addEventListener('change', checkInputs);
+  fileInput.addEventListener('change', checkInputs);
+});
+
+document
+  .getElementById('add-photo-modal')
+  .addEventListener('click', selectImage);
+
+
+
 const submitWork = async (event) => {
   event.preventDefault();
 
   const title = document.getElementById('title').value;
   const category = document.getElementById('category').value;
 
-  const url = URL + 'works';
-  const formData = new FormData();
-  formData.append('title', title);
-  formData.append('category', category);
-  formData.append('image', file);
+  if (!file || !title || !category) {
+    const error = document.getElementById('error_submit');
+    const span = document.createElement('span');
+    span.textContent = 'Verifier que tous les champs sont correct';
+    span.style.color = 'red';
+    span.style;
+    error.appendChild(span);
+    return;
+  } else {
+    const url = URL + 'works';
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('category', category);
+    formData.append('image', file);
 
-  try {
-    await fetch(url, {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      body: formData,
-    });
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      });
 
-    if (!response.ok) {
       console.log(response);
+      //  const data = await response.json();
+
+      if (!response.ok) {
+        console.log(response);
+      }
+    } catch (error) {
+      console.error('Failed to create work:', error);
     }
-  } catch (error) {
-    console.error('Failed to create work:', error);
   }
-}
+};
 
 // RUN
 
+/**
+ * Logs out the user by removing the token from local storage and updating the display of the login and logout buttons.
+ */
 const logout = () => {
   localStorage.removeItem('token');
   document.getElementById('login-url').style.display = 'block';
   document.getElementById('logout').style.display = 'none';
 }
 
-const addListeners = () => {
-  document.getElementById('logout').addEventListener('click', logout);
-  document.getElementById('close-modal').addEventListener('click', closeModal);
-  document.getElementById('open-modal').addEventListener('click', openModal);
-}
+document.getElementById('logout').addEventListener('click', logout);
+document.getElementById('close-modal').addEventListener('click', closeModal);
+document.getElementById('open-modal').addEventListener('click', openModal);
 
 // ********** MAIN ********** //
 
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('Application en cours de chargement...');
   token = localStorage.getItem('token');
 
   if (token) {
     document.getElementById('login-url').style.display = 'none';
     document.getElementById('logout').style.display = 'block';
+    document.getElementById('categories').style.display = 'none';
   } else {
+    document.getElementById('open-modal').style.display = 'none'
+    document.getElementById('modifier').style.display = 'none';
 
   }
 
@@ -209,13 +292,57 @@ document.addEventListener('DOMContentLoaded', function () {
   displayCategories();
 
   document.getElementById('imageContainer').style.display = 'none';
-})
+});
 
 document
   .getElementById('create-works-form')
   .addEventListener('submit', async (event) => {
     submitWork(event);
   });
+
+
+  // FILTER
+const filterWorks = (categoryId, button) => {
+  const worksdoc = document.getElementById('work');
+  worksdoc.innerHTML = '';
+
+  // Filtrer les travaux en fonction de l'ID de catégorie
+  const filteredWorks = works.filter((work) => work.categoryId === categoryId);
+  console.log(filteredWorks);
+  // Ajouter les éléments filtrés au DOM
+  filteredWorks.forEach((work) => {
+    const figure = document.createElement('figure');
+    figure.innerHTML = `
+        <img src="${work.imageUrl}" alt="${work.title}">
+        <figcaption>${work.title}</figcaption>
+      `;
+    worksdoc.appendChild(figure);
+  });
+
+  document.querySelectorAll('.category__btn').forEach((btn) => {
+    btn.classList.remove('active');
+  });
+  button.classList.add('active');
+  document.getElementById('all-work').classList.remove('btn-all');
+};
+
+document.getElementById('all-work').addEventListener('click', () => {
+  const worksdoc = document.getElementById('work');
+  worksdoc.innerHTML = '';
+  works.forEach((work) => {
+    const figure = document.createElement('figure');
+    figure.innerHTML = `
+        <img src="${work.imageUrl}" alt="${work.title}">
+        <figcaption>${work.title}</figcaption>
+      `;
+    worksdoc.appendChild(figure);
+  });
+
+  document.getElementById('all-work').classList.add('btn-all');
+  document.querySelectorAll('.category__btn').forEach((btn) => {
+    btn.classList.remove('active');
+  });
+});
 
 
 
